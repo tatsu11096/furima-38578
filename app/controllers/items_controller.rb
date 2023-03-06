54b58d06cdc9e8652e0f933baa6ab5816b609ae5
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2023-03-06 17:15:13 +0900

    item登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,32 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはトップページに促す
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: :index, :show
+  
+
+  
   def index
+     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  def item_params
+    params.require(:item).permit(:image,:name,:messege,:category_id,:condition_id,:shipping_charge_id,:shipping_region_id,:shipping_date_id,:price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  with_options presence: true do
+       
+         has_many:items
+  
+   with_options presence: true do
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates :password, format: { with: VALID_PASSWORD_REGEX }
 
