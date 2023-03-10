dfc6a201d56868ef50896d453dfd245897770abb
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2023-03-10 18:56:24 +0900

    rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,19 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはトップページに促す
-  before_action :authenticate_user!, except: [:index,:show]
- before_action :set_item, only: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
   def new
     @item = Item.new
   end
 
-
   def index
-    @items = Item.all.order(created_at: "DESC")
+    @items = Item.all.order(created_at: 'DESC')
   end
 
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.valid?
@@ -31,6 +30,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :message, :category_id, :condition_id, :shipping_charge_id, :shipping_region_id,
                                  :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+
   def set_item
     @item = Item.find(params[:id])
   end
