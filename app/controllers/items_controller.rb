de3c7194810a14bc0f7c8032744c94040b874272
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2023-03-10 20:33:14 +0900

    rubocop修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはトップページに促す
 
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
   # before_action :redirect,           only:   [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
   end
-
 
   def new
     @item = Item.new
