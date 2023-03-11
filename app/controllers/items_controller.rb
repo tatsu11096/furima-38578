619e3cf7c1e0a7073429a1b0e8f2e261f127dbfa
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2023-03-11 14:26:13 +0900

    削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   # ログインしていないユーザーはトップページに促す
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show,:destroy]
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
   # before_action :redirect,           only:   [:edit, :update, :destroy]
 
@@ -29,6 +29,13 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
     end
   end
 
