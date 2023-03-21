9aa64efd93b3b2bec562d937b11609b8083c80af
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2023-03-21 23:03:33 +0900

    rubocop2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,9 +19,9 @@ class ItemsController < ApplicationController
 
   def edit
     redirect_to root_path unless current_user.id == @item.user_id
-    if @item.user_id != current_user.id || @item.order != nil
-      redirect_to root_path
-    end
+    return unless @item.user_id != current_user.id || !@item.order.nil?
+
+    redirect_to root_path
   end
 
   def update
